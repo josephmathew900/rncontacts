36b80fb226ffa4c7af8992e8c15b41f085be9c31
josephmathew900 <josephmathew900@gmail.com>
2021-06-02 21:37:08 +0530

    login screen UI

---
+++ src/components/Login/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import Container from '../../components/common/Container';
+import CustomButton from '../../components/common/CustomButton';
+import Input from '../../components/common/Input';
+import styles from './styles';
+import {REGISTER} from '../../constants/routeNames';
+
+const Login = () => {
+  const {navigate} = useNavigation();
+
+  return (
+    <Container>
+      <Image
+        height={70}
+        width={70}
+        source={require('../../assets/images/logo.png')}
+        style={styles.logoImage}
+      />
+
+      <View>
+        <Text style={styles.title}>Welcome to RNContacts</Text>
+        <Text style={styles.subTitle}>Please login here</Text>
+
+        <View style={styles.form}>
+          <Input label="Username" placeholder="Enter Username" />
+          <Input
+            label="Password"
+            placeholder="Enter Password"
+            secureTextEntry
+            icon={<Text>Show</Text>}
+            iconPosition="right"
+          />
+
+          <CustomButton primary title="Submit" />
+
+          <View style={styles.createSection}>
+            <Text style={styles.infoText}>Need a new account?</Text>
+            <TouchableOpacity onPress={() => navigate(REGISTER)}>
+              <Text style={styles.linkBtn}>Register</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Container>
+  );
+};
+
+export default Login;
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,30 +1,8 @@
 import React, {useState} from 'react';
-import {Text, TextInput, View} from 'react-native';
-import Container from '../../components/common/Container';
-import CustomButton from '../../components/common/CustomButton';
-import Input from '../../components/common/Input';
+import LoginComponent from '../../components/Login';
 
 const Login = () => {
-  const [value, onChangeText] = useState('test');
-
-  return (
-    <Container>
-      <Input
-        label="Username"
-        onChangeText={text => onChangeText(text)}
-        value={value}
-      />
-      <Input
-        label="Password"
-        onChangeText={text => onChangeText(text)}
-        value={value}
-        icon={<Text>Hide</Text>}
-        iconPosition="right"
-      />
-
-      <CustomButton primary title="submit" />
-    </Container>
-  );
+  return <LoginComponent />;
 };
 
 export default Login;
