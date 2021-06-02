756905856ccd36e5998ee7ad3405ef203004f25a
josephmathew900 <josephmathew900@gmail.com>
2021-06-02 11:03:38 +0530

    custom text input

---
+++ src/components/common/Input/index.js
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import {View, Text, TextInput} from 'react-native';
+import styles from './styles';
+import colors from '../../../assets/theme/colors';
+
+const Input = ({
+  onChangeText,
+  style,
+  value,
+  label,
+  icon,
+  iconPosition,
+  error,
+  ...props
+}) => {
+  const [focused, setFocused] = useState(false);
+
+  const getFlexDirection = () => {
+    if (icon && iconPosition) {
+      if (iconPosition === 'left') return 'row';
+      else if (iconPosition === 'right') return 'row-reverse';
+    } else return 'row';
+  };
+
+  const getBorderColor = () => {
+    if (focused) {
+      return colors.primary;
+    }
+    if (error) return colors.danger;
+    else return colors.grey;
+  };
+  return (
+    <View style={styles.inputContainer}>
+      {label && <Text>{label}</Text>}
+      <View
+        style={[
+          styles.wrapper,
+          {
+            borderColor: getBorderColor(),
+            flexDirection: getFlexDirection(),
+          },
+        ]}>
+        {icon && <View style={{alignSelf: 'center'}}>{icon}</View>}
+        <TextInput
+          style={[styles.textInput, style]}
+          onChangeText={onChangeText}
+          value={value}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+          {...props}
+        />
+      </View>
+      {error && <Text style={styles.error}>{error}</Text>}
+    </View>
+  );
+};
+
+export default Input;
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Text, TextInput, View} from 'react-native';
+import Container from '../../components/common/Container';
+import Input from '../../components/common/Input';
 
 const Login = () => {
+  const [value, onChangeText] = useState('test');
+
   return (
-    <View>
-      <Text>Hi from Login</Text>
-    </View>
+    <Container>
+      <Input
+        label="Username"
+        onChangeText={text => onChangeText(text)}
+        value={value}
+      />
+      <Input
+        label="Password"
+        onChangeText={text => onChangeText(text)}
+        value={value}
+        icon={<Text>Hide</Text>}
+        iconPosition="right"
+      />
+    </Container>
   );
 };
 
