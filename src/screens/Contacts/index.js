1285612706a4621c2d4dd07d84984e03135b6445
josephmathew900 <josephmathew900@gmail.com>
2021-06-07 21:11:08 +0530

    create contact -> redirect to contact detail page

--- src/screens/Contacts/index.js
+++ src/screens/Contacts/index.js
@@ -1,5 +1,5 @@
 import {useNavigation} from '@react-navigation/core';
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useRef} from 'react';
 import {useEffect} from 'react';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import ContactsComponent from '../../components/Contacts';
@@ -9,11 +9,13 @@ import {GlobalContext} from '../../context/Provider';
 import getContacts from '../../context/actions/contacts/getContacts';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useFocusEffect} from '@react-navigation/native';
+import {CONTACT_DETAILS} from '../../constants/routeNames';
 
 const Contacts = ({navigation}) => {
-  const {setOptions, toggleDrawer} = useNavigation();
+  const {navigate, setOptions, toggleDrawer} = useNavigation();
   const [modalVisible, setModalVisible] = useState(false);
   const [sortBy, setSortBy] = useState(null);
+  const contactsRef = useRef([]);
 
   const {
     contactsState: {
@@ -39,6 +41,19 @@ const Contacts = ({navigation}) => {
       return () => {};
     }, []),
   );
+
+  useEffect(() => {
+    const prev = contactsRef.current;
+    contactsRef.current = data;
+    const newList = contactsRef.current;
+    if (newList.length - prev.length === 1) {
+      const newContact = newList.find(
+        newItem => !prev.map(prevItem => prevItem.id).includes(newItem.id),
+      );
+
+      navigate(CONTACT_DETAILS, {item: newContact});
+    }
+  }, [data.length]);
 
   useEffect(() => {
     setOptions({
